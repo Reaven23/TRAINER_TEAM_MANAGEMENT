f7ee28b38a3d61bcddc1061ff448fcd151ed9787
Reaven23 <adrien.regis@gmail.com>
2025-06-27 18:33:31 +0200

    devise views done

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,22 +1,50 @@
 class SkillsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_skill, only: [:show, :edit, :update, :destroy]
+
   def index
+    @skills = Skill.all
   end
 
   def show
   end
 
   def new
+    @skill = Skill.new
   end
 
   def create
+    @skill = Skill.new(skill_params)
+    if @skill.save
+      redirect_to @skill, notice: 'Compétence créée avec succès.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @skill.update(skill_params)
+      redirect_to @skill, notice: 'Compétence mise à jour avec succès.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @skill.destroy
+    redirect_to skills_url, notice: 'Compétence supprimée avec succès.'
+  end
+
+  private
+
+  def set_skill
+    @skill = Skill.find(params[:id])
+  end
+
+  def skill_params
+    params.require(:skill).permit(:name, :category, :description, :age_group)
   end
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,22 +1,62 @@
 class TeamsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_team, only: [:show, :edit, :update, :destroy, :players, :coaches, :parents]
+
   def index
+    @teams = Team.all
   end
 
   def show
   end
 
   def new
+    @team = Team.new
   end
 
   def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to @team, notice: 'Équipe créée avec succès.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @team.update(team_params)
+      redirect_to @team, notice: 'Équipe mise à jour avec succès.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @team.destroy
+    redirect_to teams_url, notice: 'Équipe supprimée avec succès.'
+  end
+
+  def players
+    @players = @team.players
+  end
+
+  def coaches
+    @coaches = @team.coaches
+  end
+
+  def parents
+    @parents = @team.parents
+  end
+
+  private
+
+  def set_team
+    @team = Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:name, :category, :description)
   end
 end
